08a3cd18f5b11ac90dd3b019d10d85c642af9b04
James T <jtara@tuta.io>
2017-02-10 16:08:38 -0800

    init commit 2

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,41 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.loaders.resolvers.ExternalFileHandleResolver;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.google.protobuf.*;
+import com.google.protobuf.AbstractMessage.Builder;
+
+public class MyGdxGame extends ApplicationAdapter {
+	Builder kdk;
+	SpriteBatch batch;
+	Texture img;
+	
+	TiledMap map = new TmxMapLoader(new ExternalFileHandleResolver()).load("level1.tmx");
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
