00a968ea6d466d50d9060fad55fd7800b01a2897
James T <jtara@tuta.io>
2017-02-11 14:30:40 -0800

    world background texture is now disposed

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -66,5 +66,6 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 		playerSprite.dispose();
+		world.dispose();
 	}
 }
--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -1,5 +1,6 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -7,18 +8,26 @@ import com.badlogic.gdx.math.Rectangle;
 
 public class World {
 	public Texture background;
+	
 	public Rectangle boundaries;
 	
 	public SpriteBatch batch;
 	
 	public World(String fileName) {
+		
+		// image of World background loaded as a Texture
 		background = new Texture(fileName);
-//		boundaries = new Rectangle(0, 0, width, height);
+		
+		// used to draw each Texture
 		batch = new SpriteBatch();
+		
+		// back default boundaries the size of the background image
+		boundaries = new Rectangle(0, 0, background.getWidth(), background.getHeight());
 	}
 	
 	public World(String fileName, float width, float height) {
 		this(fileName);
+		boundaries = new Rectangle(0, 0, width, height);
 	}
 	
 	public void draw() {
