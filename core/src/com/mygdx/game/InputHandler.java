98351ac582ebdd4ca91c3fcb2f66130ce7f4b0bd
lcdflare <frfabianrp@gmail.com>
2017-02-11 17:45:11 -0800

    Camera follows player sprite

--- core/src/com/mygdx/game/InputHandler.java
+++ core/src/com/mygdx/game/InputHandler.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class InputHandler {
 
@@ -9,22 +10,26 @@ public class InputHandler {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void move(Player player) {
+	public void move(Player player, OrthographicCamera camera) {
 		if(Gdx.input.isKeyPressed(Keys.DPAD_LEFT)) {
 		      player.position.x -= Gdx.graphics.getDeltaTime() * player.speed;
 		      player.sprite = player.sprites[1];
+		      camera.translate(-Gdx.graphics.getDeltaTime() * player.speed, 0, 0);
 		   }
 		   if(Gdx.input.isKeyPressed(Keys.DPAD_RIGHT)) { 
 			   player.position.x += Gdx.graphics.getDeltaTime() * player.speed;
 			   player.sprite = player.sprites[3];
+			   camera.translate(Gdx.graphics.getDeltaTime() * player.speed, 0, 0);
 		   }
 		   if(Gdx.input.isKeyPressed(Keys.DPAD_UP)) { 
 		      player.position.y += Gdx.graphics.getDeltaTime() * player.speed;
 		      player.sprite = player.sprites[0];
+		      camera.translate(0, Gdx.graphics.getDeltaTime() * player.speed, 0);
 		   }
 		   if(Gdx.input.isKeyPressed(Keys.DPAD_DOWN)) { 
 			   player.position.y -= Gdx.graphics.getDeltaTime() * player.speed;
 			   player.sprite = player.sprites[2];
+			   camera.translate(0, -Gdx.graphics.getDeltaTime() * player.speed, 0);
 		   }
 	}
 }
--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -8,7 +10,10 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 
-public class World {
+public class World implements ApplicationListener {
+	private OrthographicCamera cam;
+	private float rotationSpeed = 0.5f;
+	
 	public Texture background;
 	
 	public Rectangle boundaries;
@@ -33,6 +38,13 @@ public class World {
 		player = new Player();
 		
 		input = new InputHandler();
+		
+		float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+        cam = new OrthographicCamera(30, 30* (h/w));
+        cam.position.set(cam.viewportWidth/2f, cam.viewportHeight/2f,0);
+        cam.zoom += 15;
+        cam.update();
 	}
 	
 	public World(String fileName, float width, float height) {
@@ -41,15 +53,84 @@ public class World {
 	}
 	
 	public void draw() {
+		handleInput();
+	    cam.update();
+	    batch.setProjectionMatrix(cam.combined);
+	        
 		batch.begin();
 		batch.draw(background, 0, 0);
 		batch.draw(player.sprite, player.position.x, player.position.y);
-		input.move(player);
+		input.move(player, cam);
 		batch.end();
 	}
 	
 	public void dispose() {
 		background.dispose();
 	}
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+    public void resize(int width, int height) {
+        cam.viewportWidth = 30f;
+        cam.viewportHeight = 30f * height/width;
+        cam.update();
+    }
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void handleInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
+            cam.zoom += 0.02;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.E)) {
+            cam.zoom -= 0.02;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            cam.translate(-3, 0, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            cam.translate(3, 0, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            cam.translate(0, -3, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            cam.translate(0, 3, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.R)) {
+            cam.rotate(-rotationSpeed, 0, 0, 1);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.F)) {
+            cam.rotate(rotationSpeed, 0, 0, 1);
+        }
+
+        //cam.zoom = MathUtils.clamp(cam.zoom, 0.1f, 100/cam.viewportWidth);
+
+        //float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
+        //float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
+
+        //cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, 100 - effectiveViewportWidth / 2f);
+        //cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, 100 - effectiveViewportHeight / 2f);
+    }
 	
 }
