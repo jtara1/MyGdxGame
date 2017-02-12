b86f457f267af30dd3dd0ba35ebe36cda8a6ea35
lcdflare <frfabianrp@gmail.com>
2017-02-11 16:45:57 -0800

    One more small

--- core/src/com/mygdx/game/InputHandler.java
+++ core/src/com/mygdx/game/InputHandler.java
@@ -1,9 +1,30 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+
 public class InputHandler {
 
 	public InputHandler() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public void move(Player player) {
+		if(Gdx.input.isKeyPressed(Keys.DPAD_LEFT)) {
+		      player.position.x -= Gdx.graphics.getDeltaTime() * player.speed;
+		      player.sprite = player.sprites[1];
+		   }
+		   if(Gdx.input.isKeyPressed(Keys.DPAD_RIGHT)) { 
+			   player.position.x += Gdx.graphics.getDeltaTime() * player.speed;
+			   player.sprite = player.sprites[3];
+		   }
+		   if(Gdx.input.isKeyPressed(Keys.DPAD_UP)) { 
+		      player.position.y += Gdx.graphics.getDeltaTime() * player.speed;
+		      player.sprite = player.sprites[0];
+		   }
+		   if(Gdx.input.isKeyPressed(Keys.DPAD_DOWN)) { 
+			   player.position.y -= Gdx.graphics.getDeltaTime() * player.speed;
+			   player.sprite = player.sprites[2];
+		   }
+	}
 }
